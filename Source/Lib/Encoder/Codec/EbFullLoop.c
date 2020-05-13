c09dbcf3c6a9f2ad7f7dece64b59f71b440a047d
NaderMahdi <nader.mahdi@intel.com>
2020-05-13 14:56:32 -0700

    Remove the disallowing of the RDOQ

--- Source/Lib/Encoder/Codec/EbFullLoop.c
+++ Source/Lib/Encoder/Codec/EbFullLoop.c
@@ -1758,18 +1758,6 @@ int32_t av1_quantize_inv_quantize(
     EbBool perform_rdoq = ((md_context->md_staging_skip_rdoq == EB_FALSE || is_encode_pass) &&
         md_context->enable_rdoq);
 
-#define DISABLE_RDOQ_IN_MD 1
- #define   DISABLE_RDOQ_IN_MD_FOR_INTER 1
- #define   DISABLE_RDOQ_IN_MD_FOR_INTRA 1
-#if DISABLE_RDOQ_IN_MD
-    perform_rdoq = is_encode_pass ? perform_rdoq : 0;
-#endif
-#if DISABLE_RDOQ_IN_MD_FOR_INTER
-    perform_rdoq = is_encode_pass || !is_inter ? perform_rdoq : 0;
-#endif
-#if DISABLE_RDOQ_IN_MD_FOR_INTRA
-    perform_rdoq = is_encode_pass || !is_inter ? perform_rdoq : 0;
-#endif
     SequenceControlSet *scs_ptr = (SequenceControlSet *)pcs_ptr->scs_wrapper_ptr->object_ptr;
     if (perform_rdoq) {
         if (bit_increment || (is_encode_pass && scs_ptr->static_config.encoder_16bit_pipeline)) {
